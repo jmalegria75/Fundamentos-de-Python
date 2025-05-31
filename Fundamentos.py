de7cb33fd9f1e70145f6a737841d76f1a887f05b
jmalegria75 <jmalegria75@hotmail.com>
2025-05-31 13:01:28 -0600

    Se agregan operadors aritmeticos
    Se coloca un ejemplo de cada operador aritmético

--- Fundamentos.py
+++ Fundamentos.py
@@ -1,9 +1,34 @@
-#Operadores Aritmeticos
-print("----- Operadores aritmeticos -----")
-print("Operador suma, 5 + 6 = ", 5 + 6)
-print("Operador resta, 10 - 6 = ", 10 - 6)
-print("Operador multiplicacion, 7 * 3 = ", 7 * 3)
-print("Operador división, 7 / 3 = ", 7 / 3)
-print("Operador división entera, 7 // 3 = ", 7 // 3) 
-print("Operador modulo, 7 % 3 = ", 7 % 3) 
-print("Operador cambio de signo, - 3 = ", - 3) 
+if __name__ == "__main__":
+
+    #Operadores Aritmeticos
+    print("----- Operadores aritmeticos -----")
+    print("Operador suma, 5 + 6 = ", 5 + 6)
+    print("Operador resta, 10 - 6 = ", 10 - 6)
+    print("Operador multiplicacion, 7 * 3 = ", 7 * 3)
+    print("Operador división, 7 / 3 = ", 7 / 3)
+    print("Operador división entera, 7 // 3 = ", 7 // 3) 
+    print("Operador modulo, 7 % 3 = ", 7 % 3) 
+    print("Operador cambio de signo, - 3 = ", - 3) 
+
+    #Este es un comentario de una sola linea
+
+    ''' Los 3 apostrofes se usa para comentarios de muchas
+        líneas...
+    '''
+
+    #Operadores Lógicos
+    print("----- Operadores Logicos -----")
+    print("True and True : ", True and True)
+
+    #1. Hacer las tablas de vedad
+
+    #2. Ejemplo de cada operador de comparacion
+    #Operadores Lógicos
+    print("----- Operadores de Comparacion -----")
+    print("5 == 6 : ", 5 == 6)
+
+
+# Aquí continua con otro contexto, ya no tiene tambulador
+print("Otro mensaje.")
+
+
