189705825b0be4e769c28674e0d263482f4465bd
jmalegria75 <jmalegria75@hotmail.com>
2025-05-31 12:46:43 -0600

    Crear archivo Fundamentos.py
    Operaciones con operadores aritmeticos

---
+++ Fundamentos.py
@@ -0,0 +1,9 @@
+#Operadores Aritmeticos
+print("----- Operadores aritmeticos -----")
+print("Operador suma, 5 + 6 = ", 5 + 6)
+print("Operador resta, 10 - 6 = ", 10 - 6)
+print("Operador multiplicacion, 7 * 3 = ", 7 * 3)
+print("Operador división, 7 / 3 = ", 7 / 3)
+print("Operador división entera, 7 // 3 = ", 7 // 3) 
+print("Operador modulo, 7 % 3 = ", 7 % 3) 
+print("Operador cambio de signo, - 3 = ", - 3) 
