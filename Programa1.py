c5cb9c4d7ce193233670eadba96d8ea7e784340f
jmalegria75 <jmalegria75@hotmail.com>
2025-05-31 12:20:25 -0600

    Se cambió a multiplicacion
    Cambio a multiplicacion en lugar de suma

--- Programa1.py
+++ Programa1.py
@@ -1,4 +1,4 @@
 numero1 = 9
 numero2 = 18
-resultado = numero1 + numero2 
-print("El resultado de la suma es: ", resultado)
+resultado = numero1 * numero2 
+print("El resultado de la multiplicación es: ", resultado)
